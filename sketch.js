157d0c4504fb594c814fa29819d234982768a937
JulieToastFrill <53793305+JulieToastFrill@users.noreply.github.com>
2024-10-11 19:59:31 -0400

    hopefully made it work on ios

--- sketch.js
+++ sketch.js
@@ -354,7 +354,16 @@ function touchEnded() {
   inputPosition.y = touches.y;
   isClicked = true;
 }
+//this apparently makes it work on ios. I hope to god it does because if it doesnt im going to be livid.
+function touchStarted() {
+mouseClicked();
+return false;
+}
 
+function touchMoved() {
+mouseClicked();
+return false;
+} 
 
 function modelLoaded() {
   gameState = 0;
